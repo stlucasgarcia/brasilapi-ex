1f142c53712f05b24bfad2a146c0c0cc9d1a6fbc
Lucas <lucas@stlucasgarcia.com>
2025-08-06 19:37:13 -0300

    chore: Bump version to 0.1.4 in mix.exs and README

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ Add `brasilapi` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:brasilapi, "~> 0.1.3"}
+    {:brasilapi, "~> 0.1.4"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Brasilapi.MixProject do
     [
       app: :brasilapi,
       name: "BrasilAPI",
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.18",
       description: description(),
       package: package(),
